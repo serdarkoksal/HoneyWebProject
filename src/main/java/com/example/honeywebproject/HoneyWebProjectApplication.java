f19b419477f9879e0405d4dd754f83a954badb4a
serdarkoksal <serdarkoksalbm@gmail.com>
2022-05-30 21:37:57 +0300

    create development branch

---
+++ src/main/java/com/example/honeywebproject/HoneyWebProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.honeywebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+HoneyWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/honeywebproject/HoneyWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honeywebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
